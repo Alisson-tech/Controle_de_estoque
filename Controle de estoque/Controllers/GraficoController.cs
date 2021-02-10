e6042918f6a55e64ecb86e7bcdec612964cd25ad
Alisson-tech <alissonf.45@gmail.com>
2021-02-10 12:15:39 -0300

    Aula 4 - Criado os Controllers

---
+++ Controle de estoque/Controllers/GraficoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controle_de_estoque.Controllers
+{
+    public class GraficoController : Controller
+    {
+        // GET: Grafoco
+        public ActionResult PerdaMes()
+        {
+            return View();
+        }
+        public ActionResult EntradaSaidaMes()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controle de estoque/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controle_de_estoque.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+
+            return View();
+        }
+
+    }
+}
